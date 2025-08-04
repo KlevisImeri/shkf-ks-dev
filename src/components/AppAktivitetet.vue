1a21d767aa9a4ba0fec7e7370ac0e0b4f716e3a4
zmigj <zgjimrr@gmail.com>
2025-08-04 22:24:02 +0200

    Added PDF files.

--- src/components/AppAktivitetet.vue
+++ src/components/AppAktivitetet.vue
@@ -62,6 +62,18 @@
             </svg>
         </div>
 
+        <!-- Kuantum 5 -->
+        <img src="@/assets/Kuantum5.jpg" alt="Kuantum 5" class="journal-image" />
+        <div class="text flex download-link" @click="downloadPDF(5)">
+            Kuantum 5
+            <svg class="svgdownload" viewBox="0 0 24 24">
+                <path d="M4 4C4 3.44772 4.44772 3 5 3H14H14.5858C14.851 3 15.1054 3.10536 15.2929 3.29289L19.7071 7.70711C19.8946 7.89464 20 8.149 20 8.41421V20C20 20.5523 19.5523 21 19 21H5C4.44772 21 4 20.5523 4 20V4Z" stroke="var(--linecolor-color)" />
+                <path d="M20 8H15V3" stroke="var(--linecolor-color)" />
+                <path d="M12 9L12 17" stroke="var(--linecolor-color)" />
+                <path d="M9 14L12 17L15 14" stroke="var(--linecolor-color)" />
+            </svg>
+        </div>
+
       </div>
     </div>
   </template>
